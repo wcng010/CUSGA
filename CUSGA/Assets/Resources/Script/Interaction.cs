d9afeb4316b4140fbf16b23ed91844566013af02
HuaiMengXing <1549283498@qq.com>
2023-03-11 10:17:21 +0800

    other

--- CUSGA/Assets/Resources/Script/Interaction.cs
+++ CUSGA/Assets/Resources/Script/Interaction.cs
@@ -38,7 +38,7 @@ public class Interaction : MonoBehaviour
     {
         if (IsObj && ObjName != null)
         {
-            if (Input.GetKeyDown(KeyCode.F))
+            if (sprite.enabled && Input.GetKeyDown(KeyCode.F))
             {
                 dataList = BagManager.Instance.DataListClass.ObjectList;
                 for (int i = 0; i < dataList.Count; i++)
